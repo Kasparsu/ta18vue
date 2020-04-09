3cb467bfa04009d8c5134099d182becc81edeb82
Kaspar Suursalu <kasparsu@gmail.com>
2020-04-09 15:52:43 +0300

    Add Covid-19 api application

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,90 @@
 <template>
-    <div>
-        <tabs :els="titles">
-            
-        </tabs>
+    <div class="section">
+        <div class="columns">
+            <div class="column is-three-quarters">
+                <input class="input" type="text" placeholder="Country" v-model="search">
+            </div>
+            <div class="column is-one-quarter">
+                <div class="select">
+                    <select v-model="sortValue">
+                        <option v-for="option in sort" :value="option.value">{{option.text}}</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+
+        <div class="columns is-multiline">
+            <div class="column is-one-quarter" >
+                <country-stats :data="world"></country-stats>
+            </div>
+            <div class="column is-one-quarter" v-for="country in sortedCountries">
+                <country-stats :data="country"></country-stats>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    import Tabs from "./components/Tabs.vue";
+    import axios from 'axios';
+    import CountryStats from "./components/CountryStats.vue";
     export default {
         name: "App",
-        components: {Tabs},
+        components: {CountryStats},
+        created() {
+            axios.get('https://api.covid19api.com/summary').then(response => {
+                this.countries = response.data.Countries;
+                this.world = response.data.Global;
+                this.world.Country = 'World';
+            });
+            // example of cors error
+            // axios.get('https://avatars.dicebear.com/v2/male/a.svg').then(response => {
+            //     console.log(response);
+            // });
+        },
+        mounted() {
+
+        },
         data() {
             return {
-                titles: [
-                    {title:'Pictures', content: '<b>Some cool Image content here!</b>'},
-                    {title:'Music', content: 'Some cool music content here!'},
-                    {title:'Videos', content: 'Some cool video content here!'},
-                    {title:'Documents', content: 'Some cool document content here!'},
-                    {title:'Games', content: 'Some cool gaming content here!'},
-                ]
+                countries: [],
+                world: null,
+                search: '',
+                sort: [
+                    {value: 'NewConfirmed', text: 'New Confirmed'},
+                    {value: 'TotalConfirmed', text: 'Total Confirmed'},
+                    {value: 'TotalDeaths', text: 'Total Deaths'},
+                    {value: 'NewDeaths', text: 'New Deaths'},
+                    {value: 'NewRecovered', text: 'New Recovered'},
+                    {value: 'TotalRecovered', text: 'Total Recovered'},
+                ],
+                sortValue: ''
             }
         },
         methods: {
 
+        },
+        computed: {
+            filteredCountries(){
+                return this.countries.filter(country => {
+                    let len = this.search.length;
+                    let compareStr = country.Country.substr(0, len);
+                    return this.search.toLowerCase() === compareStr.toLowerCase();
+                });
+            },
+            sortedCountries(){
+                if(this.sortValue != '') {
+                    return this.filteredCountries.sort((a, b) => {
+                        if (a[this.sortValue] < b[this.sortValue]) {
+                            return 1;
+                        }
+                        if (a[this.sortValue] > b[this.sortValue]) {
+                            return -1;
+                        }
+                        return 0;
+                    });
+                }
+                return this.filteredCountries;
+            }
         }
     }
 </script>
---
+++ src/components/CountryStats.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">
+                {{data.Country}}
+            </p>
+        </header>
+        <div class="card-content">
+            <div class="content">
+                <table>
+                    <tbody>
+                    <tr>
+                        <td>New Confirmed</td>
+                        <td>{{data.NewConfirmed}}</td>
+                    </tr>
+                    <tr>
+                        <td>Total Confirmed</td>
+                        <td>{{data.TotalConfirmed}}</td>
+                    </tr>
+                    <tr>
+                        <td>New Deaths</td>
+                        <td>{{data.NewDeaths}}</td>
+                    </tr>
+                    <tr>
+                        <td>Total Deaths</td>
+                        <td>{{data.TotalDeaths}}</td>
+                    </tr>
+                    <tr>
+                        <td>New Recovered</td>
+                        <td>{{data.NewRecovered}}</td>
+                    </tr>
+                    <tr>
+                        <td>Total Recovered</td>
+                        <td>{{data.TotalRecovered}}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CountryStats",
+        props: ['data']
+    }
+</script>
+
+<style scoped>
+
+</style>
