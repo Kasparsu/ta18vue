5740c9bd1c1170a7fa5212185273e5e7158009dd
Kaspar Suursalu <kasparsu@gmail.com>
2020-04-09 13:39:36 +0300

    Add tabs component

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,29 @@
 <template>
-    <div class="section">
-        <button class="button is-primary" @click="modal1Active=true">Click Me!</button>
-        <modal :active="modal1Active" @close="modal1Close">
-            <p class="image is-4by3">
-                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="">
-            </p>
-        </modal>
-        <button class="button is-warning" @click="modal2Active=true">Click Me!</button>
-        <modal :active="modal2Active" @close="modal2Active=false">
-            <div class="notification is-primary">
-                Primar lorem ipsum dolor sit amet, consectetur
-                adipiscing elit lorem ipsum dolor. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Sit amet,
-                consectetur adipiscing elit
-            </div>
-        </modal>
+    <div>
+        <tabs :els="titles">
+            
+        </tabs>
     </div>
 </template>
 
 <script>
-    import Modal from "./components/Modal.vue";
+    import Tabs from "./components/Tabs.vue";
     export default {
         name: "App",
-        components: {Modal},
+        components: {Tabs},
         data() {
             return {
-                modal1Active: false,
-                modal2Active: false
+                titles: [
+                    {title:'Pictures', content: '<b>Some cool Image content here!</b>'},
+                    {title:'Music', content: 'Some cool music content here!'},
+                    {title:'Videos', content: 'Some cool video content here!'},
+                    {title:'Documents', content: 'Some cool document content here!'},
+                    {title:'Games', content: 'Some cool gaming content here!'},
+                ]
             }
         },
         methods: {
-            modal1Close(){
-                this.modal1Active = false;
-                this.modal2Active = true;
-            }
+
         }
     }
 </script>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+    <div class="tabs">
+        <ul>
+            <li v-for="(el, key) in els" :class="{'is-active': key===active}">
+                <a @click="active = key">{{el.title}}</a>
+            </li>
+        </ul>
+    </div>
+        <div class="section" v-html="els[active].content">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Tabs",
+        props: ['els'],
+        data() {
+            return {
+                active: 0
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
